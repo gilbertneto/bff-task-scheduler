96e4949faefce2b5d0800148ab341e833a92bdec
Gilberto <gilbertoneto93@gmail.com>
2025-09-11 19:53:30 -0300

    adc esqueleto projeto

---
+++ src/main/java/com/project/bff_scheduler/BffSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.project.bff_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bff_scheduler/BffSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bff_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
